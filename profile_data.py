68e577193aeb3d0a6379cc0f25eb5aa3a6d6531a
Rob Wolfe <rwolfe@portwallistechnologies.com>
2023-07-31 16:09:58 -0400

    more fussing with the data class

--- profile_data.py
+++ profile_data.py
@@ -1,8 +1,15 @@
 import json
 
-with open('thedump.json') as f:
-    the_data = json.load(f)
+class data_explorer():
+    def __init__(self,datafile:str) -> None:
+        with open(datafile) as f:
+            self.the_data:dict = json.load(f)
     
-print ([a for a in the_data])
-#print([the_data[a] for a in the_data])
+    def traverse(self,level=1):
+        return [self.the_data[a] for a in self.the_data.keys() if type(self.the_data[a]) == dict]
+        
+    
+d = data_explorer('thedump.json')
+
+print (d.traverse())
 
