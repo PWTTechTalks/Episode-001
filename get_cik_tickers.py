0c624e983348a58c25f373cac98c73ee927a76c8
Robert Wolfe <rob@portwallistechnologies.com>
2023-08-04 09:15:47 -0400

    removing debugging

--- get_cik_tickers.py
+++ get_cik_tickers.py
@@ -13,19 +13,19 @@ SEC_headers= {
 } 
 r= requests.Request(method='GET',url=f'{os.getenv("WEB_URL")}/files/company_tickers.json',headers=SEC_headers)
 s = requests.Session()
+
 req= s.send( r.prepare())
-print (req.headers)
 
 with open(file='cik_tickers.json',mode='w') as file:
     the_json = req.json()
-exit()
+
 db = Postgres()
     
 # always clear out the table first
 
 db.run(f'truncate table "{os.getenv("PG_SCHEMA")}".company_ticker')
 
-# bad for to just toss away this but ...
+# bad for us to just toss away this but ...
 
 [ db.run(  f"""insert  into "{os.getenv("PG_SCHEMA")}".company_ticker(
                 cik_str,
@@ -39,11 +39,7 @@ db.run(f'truncate table "{os.getenv("PG_SCHEMA")}".company_ticker')
 
 company_cik :str = db.one(f"""select cik_str from "{os.getenv("PG_SCHEMA")}".company_ticker where ticker = 'AES' """)
 
-print (company_cik.rjust(10,'0'))
+req = requests.get(url=f"""{os.getenv("DATA_URL")}/submissions/CIK{company_cik.rjust(10,'0')}.json""", headers=SEC_headers)
 
-req = requests.get(url=f"""{os.getenv("DATA_URL")}/submissions/CIK{company_cik.rjust(10,'0')}.json""")
-
-print (req.url)
-print (req.content)
 with open(file='thedump.json',mode='w') as file:
-    js.dump(req.json,file)
+    js.dump(req.json(),file)
